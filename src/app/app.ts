2db33f47083f511dc3b2deb6c76b5857ae5271b1
Shubham Utwal <shubhamutwal@Shubhams-MacBook-Pro.local>
2015-12-14 00:12:33 +0530

    Usage: NgFor directive 1. List of heroes displayed 2. Fav hero displayed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,29 @@
-import {bootstrap, Component} from 'angular2/angular2';
+import {bootstrap, Component, NgFor} from 'angular2/angular2';
 
 @Component({
     selector: 'my-app',
     template: `
-    <h1>{{title}}</h1> 
-    <h3>My favourite hero is: {{hero}}</h3>
-    `
+    <h1>{{ title }}</h1> 
+    <h2>My favorite hero is: {{ myHero }}</h2>
+    <p>Heroes:</p>
+    <ul>
+      <li *ng-for="#hero of heroes">
+        {{ hero }}
+      </li>
+    </ul>
+    `,
+    directives: [NgFor]
 })
 class AppComponent {
-    public title = 'Title of Heroes';
-    public hero = 'Batman';
+    title: string;
+    myHero: string;
+    heroes: string [];
+    
+    constructor() {
+        this.title = 'Title of Heroes';
+        this.heroes = ['Superman', 'Bombasto', 'Magneta', 'Tornado'];
+        this.myHero = this.heroes[0];        
+    }
  }
 
 bootstrap(AppComponent);
