945523e2c54c89f0a041db7d155dc6cdcfbb2c0d
Shubham Utwal <shubhamutwal@Shubhams-MacBook-Pro.local>
2015-12-14 00:00:22 +0530

    AppComponent declared 1. Variable initialization 2. One way data binding shown

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,8 +2,14 @@ import {bootstrap, Component} from 'angular2/angular2';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Ola! My First Angular 2 Application</h1>'
+    template: `
+    <h1>{{title}}</h1> 
+    <h3>My favourite hero is: {{hero}}</h3>
+    `
 })
-class AppComponent { }
+class AppComponent {
+    public title = 'Title of Heroes';
+    public hero = 'Batman';
+ }
 
 bootstrap(AppComponent);
