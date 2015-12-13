200253ef50c3065d8949d22ed435cb3670f067ca
Shubham Utwal <shubhamutwal@Shubhams-MacBook-Pro.local>
2015-12-14 00:25:23 +0530

    Created 'Hero' class for displaying data.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,28 +1,35 @@
 import {bootstrap, Component, NgFor} from 'angular2/angular2';
+import {Hero} from './hero';
 
 @Component({
     selector: 'my-app',
     template: `
     <h1>{{ title }}</h1> 
-    <h2>My favorite hero is: {{ myHero }}</h2>
+    <h2>My favorite hero is: {{ myHero.name }}</h2>
     <p>Heroes:</p>
     <ul>
       <li *ng-for="#hero of heroes">
-        {{ hero }}
+        {{ hero.name }}
       </li>
     </ul>
     `,
     directives: [NgFor]
 })
-class AppComponent {
+export class AppComponent {
     title: string;
-    myHero: string;
-    heroes: string [];
+    myHero: Hero;
+    heroes: Hero [];
     
     constructor() {
         this.title = 'Title of Heroes';
-        this.heroes = ['Superman', 'Bombasto', 'Magneta', 'Tornado'];
-        this.myHero = this.heroes[0];        
+        this.heroes = [
+            new Hero(1, 'Superman'),
+            new Hero(2, 'Bombasto'),
+            new Hero(3, 'Robinhood'),
+            new Hero(4, 'Shaktimaan'),
+            new Hero(5, 'Spiderman')
+        ];
+        this.myHero = this.heroes[3];        
     }
  }
 
