db5c6f7b2a482e941a7f58b5275a51251b430e08
Shubham Utwal <shubhamutwal@Shubhams-MacBook-Pro.local>
2015-12-17 00:54:52 +0530

    Implementing Two Way Binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,30 @@
-import {bootstrap, Component, NgFor} from 'angular2/angular2';
+import {bootstrap, Component, NgFor, NgModel} from 'angular2/angular2';
 import {Hero} from './hero';
 
 @Component({
     selector: 'my-app',
     template: `
     <h1>{{ title }}</h1> 
-    <h2>My favorite hero is: {{ myHero.name }}</h2>
-    <p>Heroes:</p>
+    <h2>Hero Details: </h2>
+    <div>
+        <h4>Hero Name: {{ myHero.name }}</h4>
+        <h4>Hero ID Num: {{ myHero.id }}</h4>
+    </div>
+    <p>Other Heroes:</p>
     <ul>
       <li *ng-for="#hero of heroes">
         {{ hero.name }}
       </li>
     </ul>
+    <div>
+        <label>Name: </label>
+        <div>
+            <input  placeholder="name">
+        </div>
+    </div>
+    
     `,
-    directives: [NgFor]
+    directives: [NgFor, NgModel]
 })
 export class AppComponent {
     title: string;
